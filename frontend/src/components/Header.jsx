ba1fe98638d9e5eb282082d0924d388aa85ef5b0
sathya-teja <134315505+sathya-teja@users.noreply.github.com>
2025-04-26 21:37:21 +0530

    Add project files

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
+        {/* ------left side------ */}
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+            <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold'>
+                Book Appointment <br /> With Trusted Doctors
+            </p>
+            <div>
+                <img src={assets.group_profiles} alt="" />
+                <p>Simply browse through our extensive list of trusted doctors, <br /> schedule your appointment hassle-free</p>
+            </div>
+
+            <a href=""> 
+                Book Appointment <img src={assets.arrow_icon} alt="" />
+            </a>
+        </div>
+        {/* ------right side----- */}
+        <div>
+            <img src={assets.header_img} alt="" />
+            
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#5f6FFF', // your custom primary color
+        },
+      },
+    },
+    plugins: [],
+  }
+  
